fa6d5f14b6982e1302e53f97ce5ed723ff419733
Pidkovych Vladyslav <vladyslavpidkovych@gmail.com>
2016-11-05 15:51:01 +0200

    XML-documentation of all public types and members of the class-library in code

--- PMC/Collections.cs
+++ PMC/Collections.cs
@@ -6,8 +6,18 @@ using System.Threading.Tasks;
 
 namespace PMC
 {
+    /// <summary>
+    /// Collection of Points
+    /// </summary>
+    /// <typeparam name="T">Any numeric types</typeparam>
+    /// <seealso cref="PMC.Collector{PMC.IPoint}" />
     public class Position<T> : Collector<IPoint> where T : struct, IComparable, IFormattable, IConvertible, IComparable<T>, IEquatable<T>
     {
+        /// <summary>
+        /// Adds element into collection <see cref="T:System.Collections.Generic.ICollection`1" />.
+        /// </summary>
+        /// <param name="item">Object to add into collection <see cref="T:System.Collections.Generic.ICollection`1" />.</param>
+        /// <exception cref="ArgumentException"></exception>
         public override void Add(IPoint item)
         {
             if(collector.Count!=0)
@@ -28,8 +38,18 @@ namespace PMC
         }
     }
 
+    /// <summary>
+    /// Collection of Positions
+    /// </summary>
+    /// <typeparam name="T">Any numeric types</typeparam>
+    /// <seealso cref="PMC.Collector{PMC.Position{T}}" />
     public class Matrix<T> : Collector<Position<T>> where T : struct, IComparable, IFormattable, IConvertible, IComparable<T>, IEquatable<T>
     {
+        /// <summary>
+        /// Adds element into collection <see cref="T:System.Collections.Generic.ICollection`1" />.
+        /// </summary>
+        /// <param name="item">Object to add into collection <see cref="T:System.Collections.Generic.ICollection`1" />.</param>
+        /// <exception cref="ArgumentException"></exception>
         public override void Add(Position<T> item)
         {
             if (collector.Count != 0)
@@ -50,8 +70,18 @@ namespace PMC
         }
     }
 
+    /// <summary>
+    /// Collection of Matrixes
+    /// </summary>
+    /// <typeparam name="T">Any numeric types</typeparam>
+    /// <seealso cref="PMC.Collector{PMC.Matrix{T}}" />
     public class Container<T> : Collector<Matrix<T>> where T : struct, IComparable, IFormattable, IConvertible, IComparable<T>, IEquatable<T>
     {
+        /// <summary>
+        /// Adds element into collection <see cref="T:System.Collections.Generic.ICollection`1" />.
+        /// </summary>
+        /// <param name="item">Object to add into collection <see cref="T:System.Collections.Generic.ICollection`1" />.</param>
+        /// <exception cref="ArgumentException"></exception>
         public override void Add(Matrix<T> item)
         {
             if (collector.Count != 0)
@@ -72,8 +102,18 @@ namespace PMC
         }
     }
 
+    /// <summary>
+    /// Collection of Containers
+    /// </summary>
+    /// <typeparam name="T">Any numeric types</typeparam>
+    /// <seealso cref="PMC.Collector{PMC.Container{T}}" />
     public class Containers<T> : Collector<Container<T>> where T : struct, IComparable, IFormattable, IConvertible, IComparable<T>, IEquatable<T>
     {
+        /// <summary>
+        /// Adds element into collection <see cref="T:System.Collections.Generic.ICollection`1" />.
+        /// </summary>
+        /// <param name="item">Object to add into collection <see cref="T:System.Collections.Generic.ICollection`1" />.</param>
+        /// <exception cref="ArgumentException"></exception>
         public override void Add(Container<T> item)
         {
             if (collector.Count != 0)
--- PMC/Collector.cs
+++ PMC/Collector.cs
@@ -7,54 +7,110 @@ using System.Threading.Tasks;
 
 namespace PMC
 {
+    /// <summary>
+    /// Generic abstract class of collection
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <seealso cref="System.Collections.Generic.ICollection{T}" />
     public abstract class Collector<T>: ICollection<T>
     {
         protected List<T> collector;
         private bool isReadOnly;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Collector{T}"/> class.
+        /// </summary>
         public Collector()
         {
             collector = new List<T>();
         }
 
+        /// <summary>
+        /// Indexer that gets or sets the <see cref="T"/> at the specified index.
+        /// </summary>
+        /// <value>
+        /// <see cref="T"/>.
+        /// </value>
+        /// <param name="index">Index.</param>
         public T this[int index]
         {
             get { return collector[index]; }
             set { collector[index] = value; }
         }
 
+        /// <summary>
+        /// Adds element into collection <see cref="T:System.Collections.Generic.ICollection`1" />.
+        /// </summary>
+        /// <param name="item">Object to add into collection <see cref="T:System.Collections.Generic.ICollection`1" />.</param>
         public abstract void Add(T item);
 
+        /// <summary>
+        /// Deletes all elements from collection <see cref="T:System.Collections.Generic.ICollection`1" />.
+        /// </summary>
         public void Clear()
         {
             collector.Clear();
         }
 
+        /// <summary>
+        /// Determines whether the collection <see cref="T:System.Collections.Generic.ICollection`1" /> contains specified value.
+        /// </summary>
+        /// <param name="item">Object needed to be found into <see cref="T:System.Collections.Generic.ICollection`1" />.</param>
+        /// <returns>
+        /// Value true, if object <paramref name="item" /> is found into <see cref="T:System.Collections.Generic.ICollection`1" />; else — returns false.
+        /// </returns>
         public bool Contains(T item)
         {
             return collector.Contains(item);
         }
 
+        /// <summary>
+        /// Copies elements <see cref="T:System.Collections.Generic.ICollection`1" /> into array <see cref="T:System.Array" />, starting from defined index <see cref="T:System.Array" />.
+        /// </summary>
+        /// <param name="array">Array <see cref="T:System.Array" />into which copies element of <see cref="T:System.Collections.Generic.ICollection`1" />.Array <see cref="T:System.Array" /> must start indexation from zero.</param>
+        /// <param name="arrayIndex">Index that counts from zero into <paramref name="array" />showing begin of copy.</param>
         public void CopyTo(T[] array, int arrayIndex)
         {
             collector.CopyTo(array, arrayIndex);
         }
 
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection.
+        /// </summary>
+        /// <returns>
+        /// Interface <see cref="T:System.Collections.Generic.IEnumerator`1" /> which can be used to iterate through the collection.
+        /// </returns>
         public IEnumerator<T> GetEnumerator()
         {
             return collector.GetEnumerator();
         }
 
+        /// <summary>
+        /// Removes the first instance of the specified object from the collection <see cref="T:System.Collections.Generic.ICollection`1" />.
+        /// </summary>
+        /// <param name="item">The object to remove from the collection <see cref="T:System.Collections.Generic.ICollection`1" />.</param>
+        /// <returns>
+        /// Value true, if element <paramref name="item" /> is succesfully deleted from <see cref="T:System.Collections.Generic.ICollection`1" />, else — returns false.This method also returns false, if the parameter <paramref name = "item" /> can not be found in the source interface <see cref = "T: System.Collections.Generic.ICollection`1" />.
+        /// </returns>
         public bool Remove(T item)
         {
             return collector.Remove(item);
         }
 
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection.
+        /// </summary>
+        /// <returns>
+        /// Object <see cref="T:System.Collections.IEnumerator" /> which can be used to iterate through the collection.
+        /// </returns>
         IEnumerator IEnumerable.GetEnumerator()
         {
             return collector.GetEnumerator();
         }
 
+        /// <summary>
+        /// Gets the number of elements contained in the interface <see cref="T:System.Collections.Generic.ICollection`1" />.
+        /// </summary>
         public int Count
         {
             get
@@ -63,6 +119,9 @@ namespace PMC
             }
         }
 
+        /// <summary>
+        /// Gets a value indicating whether the <see cref = "T: System.Collections.Generic.ICollection`1" /> is available for read-only.
+        /// </summary>
         public bool IsReadOnly
         {
             get
